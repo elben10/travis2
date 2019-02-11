ffab5cedb02597c932fca7669c8d8acfaa523f8c
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-02-11 16:34:11 +0100

    Update windows.sh

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -1,6 +1,6 @@
 if [ "${PYTHON_VERSION:0:3}" == "3.8" ]
 then
-  choco install python --version ${PYTHON_VERSION} --pre
+  choco install python --version 3.8.0-a1 --pre
 else
   choco install python --version ${PYTHON_VERSION}
 fi
