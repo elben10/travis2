6dfe6a72ddcef6bd50f66b3ea39193507e748701
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-02-11 17:15:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,23 @@ requirements = [
     "pandas",
 ]
 
+setup_requirements [
+    "pytest-runner"
+]
+
+test_requirements = [
+    "pytest"
+]
+
 setup(
-    name = 'MyPackageName',
-    version = '1.0.0',
-    url = 'https://github.com/mypackage.git',
-    author = 'Author Name',
-    author_email = 'author@gmail.com',
-    description = 'Description of my package',
-    packages = find_packages(),    
+    name='MyPackageName',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
     install_requires=requirements,
+    setup_requires=setup_requirements,
+    test_requires=test_requirements,
 )
